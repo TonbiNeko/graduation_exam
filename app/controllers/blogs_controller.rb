e56fdd3f3d709861e52c6d065fa041c73e1fe8a1
Junko Mikami <junkomikami312nz@gmail.com>
2021-02-17 21:53:45 +0900

    Create edit,update action and edit view

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  #before_action :set_blog, only: [:]
+  before_action :set_blog, only: %i[ edit update ]
 
   def index
     @blogs = Blog.all
@@ -14,12 +14,26 @@ class BlogsController < ApplicationController
     if @blog.save
       redirect_to blogs_path, notice: "新しく投稿しました"
     else
-      render :new
+      render :new, notice: "投稿出来ませんでした"
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "編集が完了しました"
+    else
+      render :edit, notice: "編集出来ませんでした"
     end
   end
 
 private
   def blog_params
     params.require(:blog).permit(:title, :content, :image)
+  end
+
+  def set_blog
+    @blog = Blog.find(params[:id])
   end
 end
