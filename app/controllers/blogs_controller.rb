80ca9528cbc026947bdbe023ddb86d7f7c8e01c7
Junko Mikami <junkomikami312nz@gmail.com>
2021-02-17 21:42:43 +0900

    Create new,create action and new view

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,25 @@
 class BlogsController < ApplicationController
+  #before_action :set_blog, only: [:]
+
   def index
     @blogs = Blog.all
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blogs_path, notice: "新しく投稿しました"
+    else
+      render :new
+    end
+  end
+
+private
+  def blog_params
+    params.require(:blog).permit(:title, :content, :image)
   end
 end
