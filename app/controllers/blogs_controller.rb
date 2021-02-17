12451ec3424cf3abe7db9bca535cc64207ba1585
Junko Mikami <junkomikami312nz@gmail.com>
2021-02-17 22:10:06 +0900

    Create destroy action

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: %i[ edit update show ]
+  before_action :set_blog, only: %i[ edit update show destroy ]
 
   def index
     @blogs = Blog.all
@@ -12,9 +12,9 @@ class BlogsController < ApplicationController
   def create
     @blog = Blog.new(blog_params)
     if @blog.save
-      redirect_to blogs_path, notice: "新しく投稿しました"
+      redirect_to blogs_path, notice: "素晴らしいです！またごみ拾い記録をみんなにシェアしてくださいね！"
     else
-      render :new, notice: "投稿出来ませんでした"
+      render :new, notice: "記録を投稿出来ませんでした"
     end
   end
 
@@ -29,6 +29,11 @@ class BlogsController < ApplicationController
   end
 
   def show; end
+
+  def destroy
+    @blog.destroy
+    redirect_to blogs_path, notice: "投稿が削除されました"
+  end
 
 private
   def blog_params
