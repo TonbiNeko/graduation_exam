9e437405d33bf1cbd09e6a676b62533e6ce929a7
Junko Mikami <junkomikami312nz@gmail.com>
2021-02-19 11:17:28 +0900

    Add image column to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  mount_uploader :image, ImageUploader
   has_many :blogs
   validates :name, presence: true
   # Include default devise modules. Others available are:
