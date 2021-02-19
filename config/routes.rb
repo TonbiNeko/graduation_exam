95b167cae1da7ba27582899b3fb8c82566d7b439
Junko Mikami <junkomikami312nz@gmail.com>
2021-02-19 14:28:53 +0900

    Create places routing, controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :places
-  # resources :users
-  # devise_for :users
+  devise_for :places, controllers: { registrations: 'places/registrations',
+                                    sessions: 'places/sessions',
+                                    passwords:     'places/passwords' }
   devise_for :users, controllers: { registrations: 'users/registrations',
-                                    sessions: 'users/sessions'}
+                                    sessions: 'users/sessions',
+                                    passwords: 'users/passwords'}
   resources :users
   root 'blogs#index'
   resources :blogs do
