d6e628c4468e2312817c54cb3e2544249f3f0a7d
Junko Mikami <junkomikami312nz@gmail.com>
2021-02-19 10:10:31 +0900

    Associate user and blogs

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,6 +2,7 @@ class Blog < ApplicationRecord
   mount_uploader :image, ImageUploader
   validates :title, presence: true, length: { in: 1..100 }
   validates :content, presence: true, length: { in: 1..1000 }
+  belongs_to :user
   has_many :blog_images, dependent: :destroy
   accepts_nested_attributes_for :blog_images, 
                                  allow_destroy: true,
