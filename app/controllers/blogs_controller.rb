7f2ac25018ab2594128248abcecf05bbf6c9c663
Junko Mikami <junkomikami312nz@gmail.com>
2021-02-19 10:26:39 +0900

    Add user name to index and show page of blog views

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -17,7 +17,7 @@ class BlogsController < ApplicationController
   end
 
   def create
-    @blog = Blog.new(blog_params)
+    @blog = current_user.blogs.build(blog_params)
     if params[:back]
       render :new
     else
@@ -30,7 +30,7 @@ class BlogsController < ApplicationController
   end
 
   def confirm
-    @blog = Blog.new(blog_params)
+    @blog = current_user.blogs.build(blog_params)
     render :new if @blog.invalid?
   end
 
