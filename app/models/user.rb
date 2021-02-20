33578d1ea4c7b72e801456ee16434737720da3a9
Junko Mikami <junkomikami312nz@gmail.com>
2021-02-21 07:35:43 +0900

    Associate favorite,blog and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   mount_uploader :image, ImageUploader
   has_many :blogs
   validates :name, presence: true
+  has_many :favorites, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
