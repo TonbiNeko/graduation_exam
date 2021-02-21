b48beb4aa4339f8f04389a5054923837eee8aa30
Junko Mikami <junkomikami312nz@gmail.com>
2021-02-21 11:58:15 +0900

    Associate comment and user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_21_013703) do
+ActiveRecord::Schema.define(version: 2021_02_21_025419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,9 @@ ActiveRecord::Schema.define(version: 2021_02_21_013703) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["blog_id"], name: "index_comments_on_blog_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "favorites", force: :cascade do |t|
@@ -103,5 +105,6 @@ ActiveRecord::Schema.define(version: 2021_02_21_013703) do
   add_foreign_key "blog_images", "blogs"
   add_foreign_key "blogs", "users"
   add_foreign_key "comments", "blogs"
+  add_foreign_key "comments", "users"
   add_foreign_key "rules", "places"
 end
