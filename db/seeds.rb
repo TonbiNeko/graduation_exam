92fe539a1a6ff78309c7593b79f265c5d7e92b62
Junko Mikami <junkomikami312nz@gmail.com>
2021-02-21 13:29:16 +0900

    Create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,50 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+User.create!(
+  name: 'bibibi',
+  email: 'bibibi@example.com',
+  password: 'umeboshi',
+  password_confirmation: 'umeboshi'
+)
+
+Place.create!(
+  email: 'nasu@example.com',
+  address: 'なすけん那須市,那須町',
+  name: 'nasuoyaji',
+  password: 'umeboshi',
+  password_confirmation: 'umeboshi'
+).build_rule(comment: "love my bi").save!
+
+5.times do
+  Place.create!(
+    email: Faker::Internet.email,
+    address: Faker::Address.state,
+    name: Faker::Games::Pokemon.name,
+    password: 'absolutegarbo',
+    password_confirmation: 'absolutegarbo',
+  ).build_rule(comment: "love my bi").save!
+end
+
+2.times do
+  User.create!(
+    name: Faker::JapaneseMedia::Doraemon.character,
+    email: Faker::Internet.email,
+    password: 'umeboshi',
+    password_confirmation: 'umeboshi'
+  )
+end
+
+User.first.blogs.create!(
+  title: Faker::Games::WorldOfWarcraft.quote,
+  content: "#{Faker::Games::WorldOfWarcraft.quote},
+            #{Faker::Games::WorldOfWarcraft.quote},
+            #{Faker::Games::WorldOfWarcraft.quote}!!!"
+)
+
+# blog1 = User.second.blog.new(...)
+# blog1.save!
+
+User.first.comments.create!(
+  blog_id: Blog.first.id,
+  content: Faker::Games::WorldOfWarcraft.race
+)
