e7981755e5a43080b79b07a5f01e128e73b0c81b
Junko Mikami <junkomikami312nz@gmail.com>
2021-02-23 18:51:25 +0900

    Add pagenation to blog index,place index and user user_blogs

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,7 @@
 class PlacesController < ApplicationController
   before_action :set_q, only: [:index, :search]
   def index
-    @places = Place.all
+    @places = Place.sort_desc.page(params[:page]).per(8)
   end
 
   def show
